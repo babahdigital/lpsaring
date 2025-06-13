fe047718a3e63b603eed79d7fc33f73f7e2ec940
babahdigital <babahdigital@gmail.com>
2025-06-13 02:14:38 +0000

    Jun 13, 2025, 10:14 AM

---
+++ backend/migrations/versions/55bc4a16f788_seed_default_package_profile.py
@@ -0,0 +1,24 @@
+"""Seed default package profile
+
+Revision ID: 55bc4a16f788
+Revises: 779e989a2bd7
+Create Date: 2025-06-13 02:18:05.989303
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '55bc4a16f788'
+down_revision = '779e989a2bd7'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
