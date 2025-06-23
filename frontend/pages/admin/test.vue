1fe8bf6a6ff9ee4d8a202511c444df8c488441c0
babahdigital <babahdigital@gmail.com>
2025-06-23 20:04:10 +0000

    Jun 24, 2025, 4:04 AM

--- frontend/pages/admin/test.vue
+++ frontend/pages/admin/test.vue
@@ -1,16 +1,16 @@
 <template>
   <div>
     <h3>Test Icons</h3>
-    <VIcon icon="tabler:check" size="x-large" />
-    <p>Icon "tabler:check" should be above.</p>
+    <VIcon icon="tabler:circle-check" size="x-large" />
+    <p>Icon "tabler:circle-check" should be above.</p>
 
     <VIcon icon="complete" size="x-large" />
-    <p>Icon "complete" (tabler:check alias) should be above.</p>
+    <p>Icon "complete" (tabler:circle-check alias) should be above.</p>
 
     <VIcon icon="mdi-check" size="x-large" />
     <p>Icon "mdi-check" should be above.</p>
 
-    <span class="tabler-check custom-icon" style="font-size: 48px;"></span>
-    <p>Icon "tabler-check" via span should be above.</p>
+    <span class="tabler-circle-check custom-icon" style="font-size: 48px;"></span>
+    <p>Icon "tabler-circle-check" via span should be above.</p>
   </div>
 </template>
