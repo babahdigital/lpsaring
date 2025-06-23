ad413fc9e4569e8c08b6e4f8f85db248526ce50a
babahdigital <babahdigital@gmail.com>
2025-06-23 13:36:00 +0000

    Jun 23, 2025, 9:36 PM

--- frontend/pages/admin/settings/general.vue
+++ frontend/pages/admin/settings/general.vue
@@ -127,7 +127,8 @@ onMounted(async () => {
       title: `Gagal Menyimpan (Error ${String(e.statusCode ?? '422')})`, // Baris 145: Mengatasi "Unexpected any value in conditional"
       text: errorDetails,
     })
-  } finally {
+  }
+  finally {
     isSaving.value = false
   }
 })
