f85a6be96fe5d86ac0fdec4de19d9fb76ff6b132
babahdigital <babahdigital@gmail.com>
2025-06-23 19:33:09 +0000

    Jun 24, 2025, 3:33 AM

--- frontend/plugins/vuetify/iconify-adapter.ts
+++ frontend/plugins/vuetify/iconify-adapter.ts
@@ -5,16 +5,7 @@ import type { IconSet, IconProps } from 'vuetify'
 
 const IconifyVuetifyAdapter: IconSet = {
   component: (props: IconProps) => {
-    // Pastikan props.icon adalah string.
-    // Jika props.icon adalah undefined, kita bisa memberikan string kosong atau ikon default sebagai fallback.
-    // Memberikan string kosong mungkin akan merender ikon kosong, yang lebih baik daripada error tipe.
-    const iconName = props.icon ? String(props.icon) : '' // Konversi ke string atau gunakan string kosong jika undefined
-
-    // Penting: IconifyIcon juga bisa menerima objek IconifyIcon (untuk data JSON ikon langsung),
-    // namun dalam konteks Vuetify dengan alias, biasanya yang datang adalah string.
-    // Jika ada kemungkinan props.icon adalah objek IconifyIcon yang sudah diproses,
-    // maka perlu penanganan lebih lanjut, tetapi untuk alias 'tabler:x', itu akan menjadi string.
-
+    const iconName = props.icon ? String(props.icon) : ''
     return h(IconifyIcon, { icon: iconName })
   },
 }
